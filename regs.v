f5547575283056280f88d4b0a4e136b6ca6846ce
jaypiper <jaypiper@users.noreply.github.com>
2019-12-19 16:25:24 +0800

    regfile

--- regs.v
+++ regs.v
@@ -3,9 +3,12 @@ module regs(  // chenlu: 寄存器模块，输入写数据
 	input [31:0] write_data; 		//写数据
 	input [4:0]  write_reg;  		//写入寄存器的id
 	input wire write_en;          //写数据使能端, 高电平有效
-	input [31:0] read_data;  		//读数据
-	input [4:0]  read_reg;	 		//读寄存器的id
-	input wire read_en;  			//读数据使能端，高电平有效
+	input [31:0] read_data1;  		//读数据
+	input [4:0]  read_reg1;	 		//读寄存器的id
+	input wire read_en1;  			//读数据使能端，高电平有效
+	input [31:0] read_data2;  		
+	input [4:0]  read_reg2;	 		
+	input wire read_en2;  			
 	output [31:0] read_out_data; 	//读出数据
 );  
 /* 问题：读写数据可能发生冲突，暂时通过不同的时钟沿避免冲突，之后考虑如何修改  */
