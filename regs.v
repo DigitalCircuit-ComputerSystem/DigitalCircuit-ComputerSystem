3ab35a655dc8d234d2ccf17d2d029bae61860380
jaypiper <jaypiper@users.noreply.github.com>
2019-12-19 16:28:42 +0800

    regflie

--- regs.v
+++ regs.v
@@ -3,13 +3,12 @@ module regs(  // chenlu: 寄存器模块，输入写数据
 	input [31:0] write_data; 		//写数据
 	input [4:0]  write_reg;  		//写入寄存器的id
 	input wire write_en;          //写数据使能端, 高电平有效
-	input [31:0] read_data1;  		//读数据
 	input [4:0]  read_reg1;	 		//读寄存器的id
-	input wire read_en1;  			//读数据使能端，高电平有效
-	input [31:0] read_data2;  		
+	input wire read_en1;  			//读数据使能端，高电平有效 		
 	input [4:0]  read_reg2;	 		
 	input wire read_en2;  			
-	output [31:0] read_out_data; 	//读出数据
+	output [31:0] read_data1_o; 	//读出数据
+	output [31:0] read_data2_o;
 );  
 /* 问题：读写数据可能发生冲突，暂时通过不同的时钟沿避免冲突，之后考虑如何修改  */
 reg[31:0] all_reg[31:0];    //cpu内部32个寄存器
@@ -22,8 +21,11 @@ always @(posedge clk) begin
 end
 
 always @(negedge clk) begin  
-	if(read_en) begin
-		read_out_data <= all_reg[read_reg];
+	if(read_en1) begin
+		read_data1_o <= all_reg[read_reg1];
+	end
+	if(read_en2) begin
+		read_data2_o <= all_reg[read_reg2];
 	end
 end
 
