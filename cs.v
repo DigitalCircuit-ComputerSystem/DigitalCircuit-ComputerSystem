f8b713ed102e53d919ba5d6734d08b782bdb29cc
jaypiper <jaypiper@users.noreply.github.com>
2019-12-22 17:02:11 +0800

    os

--- cs.v
+++ cs.v
@@ -51,15 +51,64 @@ module cs(
 	inout 		          		FPGA_I2C_SDAT
 );
 
+wire [7:0]fsmin;
+reg [23:0]vgadata1;
+wire [23:0]vgadata;
+wire [9:0]vgahaddr;
+wire [9:0]vgavaddr;
 
 
 
+reg[8:0] allchar[4095:0];  //12位，所有字符对应数据
+wire[4:0] ascv;			//列坐标
+wire[6:0] asch;  //行坐标  位置通过{ascv,asch}标识
+wire[4:0] markv; //光标			
+wire[6:0] markh;
+reg[7:0] asccode;
+reg[5:0]pos;   //vga扫描的行对应字符；
+reg[3:0]offset; //行字符偏移
+reg [7:0]vga_asc;
+reg[8:0]linedata;
+wire fsmen;
+wire [32:0] pc;
+reg [31:0] intr;  //执行指令
+
+initial begin
+	asc[0][0]=36;
+	vga_sync_n = 0;
+	$readmemh("D:/program/FPGA/11/vga_font.txt", allchar, 0, 4095);
+end
 
 
 
+fsm myfsm(.clk(fsmclk), .data(fsmdata), .asc(fsmin),.en(fsmen));   //当前存数据的地址
+
+//hel_rom
+//fib_rom
+mips_os os0(.clk(cs_clk), .addr(pc[9:0]), .q(intr));
+cpu cpu0(.clk(cs_clk), .inst(intr),.pc(pc),.raddr(memaddr),.rdata(rdata),.wren(wren),.wdata(mem));
+memery memery0(.address_a(memaddr), .data_a(mem_data), .wren_a(wren),.data_a(wdata), .q_a(rdata),
+					.address_b(0x2000|{vgavaddr[8:4]pos[5:0]}),wren_b(1'b0), q_b(vga_asc), clock_b(vga_clk));
 
 
+always @(negedge vga_clk) begin
+	 linedata = allchar[vga_asc,vgavaddr[3:0]}];
+	 vgadata1 = linedata[offset]? 24'hFFFFFF: 24'h000000;
+end
+assign vgadata = vgadata1;
 
+vga_ctrl my_vga(.pclk(vga_clk), .reset(reset), .vga_data(vgadata), .h_addr(vgahaddr), 
+			.v_addr(vgavaddr),.boffset(offset),.hblock(pos), .hsync(hs), .vsync(vs),
+			.valid(blank_n), .vga_r(vga_r), .vga_g(vga_g), .vga_b(vga_b));
+
+cpu(.pc(rom_addr), .rom_data_i(rom_data), .waddr(waddr), .wdata(wdata), .rdata(rdata), .raddr(raddr));
+			
+always @(posedge fsmclk) begin
+	//if(fsmen)
+		asc[markv[4:0]][0]=36;
+		asc[ascv[4:0]][asch[6:0]]=fsmin;
+
+end
 
 
 
--- fsm.v
+++ fsm.v
@@ -1,30 +1,27 @@
-module fsm(clk,data,asc,ascv,asch,en,markh,markv); 
-input clk, data;
+module fsm( 
+input clk,
+input data,
 //output wire[13:0]asc_show,pred_show,cou_show;
+output reg[7:0]asc,
+output reg en
+);
+reg[7:0] pred;
 reg [11:0]cou;
 reg [7:0]mem [255:0];
-output reg[7:0]asc;
-output reg en;
-reg[7:0] pred;
-output reg[6:0] asch;			//列坐标
-output reg[4:0] ascv;  //行坐标  ，将cou进行转换   两个用于上层对数据进行写入
-output reg[6:0] markh;		
-output reg[4:0] markv;
 reg [7:0]nowd;
 parameter [4:0] ini = 5'b00000, in0 = 5'b00001, in1 = 5'b00011, in2 = 5'b00010,
 					 in3 = 5'b00110, in4 = 5'b00111, in5 = 5'b00101, in6 = 5'b00100,
 					 in7 = 5'b01100, jud = 5'b01101, fin = 5'b01111;
 parameter [2:0] dep0 = 3'b000, dep1 = 3'b001, dep2 = 3'b011, outdep1 = 3'b010, outdep2 = 3'b110;
-parameter vbound = 30;
-parameter hbound = 70;   //我没有加超过vbound的判断
-reg[6:0] line_end[29:0]; //用于标识每一行的最后字符的位置(光标位置)
-reg[11:0] mark;  //前5个标识纵坐标，后7个标识横坐标  //用于标识当前位置（光标位置）
+
+
 reg [4:0]nows;
 
  reg[1:0]indi;  //0:正常，1:shift, 2:ctrl
  reg[2:0]con;
 reg upper;
 reg conen;
+reg[7:0] line_char[69:0];
 initial 
 begin
   cou = 0;
@@ -36,11 +33,12 @@ begin
   ascv = 0;
   markh = 1;
   markv = 0;
+  en = 0;
   $readmemh("D:/program/FPGA/8/mem.txt", mem, 0, 255);
 end
-
+//支持程序：输入异常(8), hel(9): 输出hello world; fib(10): 计算斐波那契数
 always @(negedge clk) begin
-	//en = 0;
+	en = 0;
 	//conen <= 0;
 	case(nows)
 		ini: begin nows <= in0; conen <= 0;end
@@ -59,110 +57,16 @@ always @(negedge clk) begin
 				case(con)
 			  dep0: begin   //未输入数据：四个灯灭
 						pred = nowd;
-						if(pred == 8'h66) begin   //删除  
-								asc = 0;
-								if({markv,markh} != 1) begin
-										if(markh == 1) begin 
-												markv = markv-1;
-												markh = line_end[markv]; 
-										end
-										else markh = markh-1;
-								end 
-								asch <= markh; ascv <= markv;
-								end
-								
+						asc = mem[pred];
 						
-						else if(pred == 8'h5a) begin //无需写入因此ascv,h保持不变
-								asc <= 0;
-								asch <= markh; ascv <= markv;
-								line_end[markv] = markh;  //记录光标位置
-								markh = 1;
-								markv = markv+1;
-						end
-						else if(pred == 8'h12)begin indi = 1;end
-						else if(pred == 8'h58)begin indi = 2;upper = ~upper; end
-
-						else begin
-						asch <= markh; ascv <= markv;  //写入和删除的定位不一样
-								asc = mem[pred];
-								if(markh == 70) begin
-									line_end[markv] = markh;
-									markh = 1;
-									markv = markv+1;
-								end
-								else markh = markh + 1;
-								if(asc >= 8'h61 && asc <= 8'h7a) asc <= upper? asc: asc - 32;
-								else if(asc >= 8'h41 && asc <= 8'h5a) asc <= upper? asc: asc + 32;
-								
-						end
 						con <= dep1;
-				
 						end
 						  dep1: begin  
 									if(nowd == 8'hf0) begin con <= outdep1; end
-									else if(indi == 1) begin 
-										if(pred != nowd) begin 
-											pred = nowd;con <= dep2;upper = ~upper;
-											asc = mem[pred];
-											asch <= markh; ascv <= markv;
-												markh = markh+1;
-												if(markh == 70) begin
-													line_end[markv] = markh;
-													markh = 1;
-													markv = markv+1;
-												end
-												if(asc >= 8'h61 && asc <= 8'h7a) asc <= upper? asc: asc - 32;
-												else if(asc >= 8'h41 && asc <= 8'h5a) asc <= upper? asc: asc + 32;
-											
-											end
-									end
-									else if(indi == 2) ;
-									else begin 
-										pred = nowd;
-										if(pred == 8'h66) begin   //删除  
-											if({markv,markh} != 0) begin
-													if(markh == 0) begin 
-															markv = markv-1;
-															markh = line_end[markv]; 
-													end
-													else markh = markh-1;
-											end
-											asch <= markh; ascv <= markv;
-										end
-										else begin
-											asc = mem[pred];
-											asch <= markh; ascv <= markv;
-												markh = markh+1;
-												if(markh == 70) begin
-													line_end[markv] = markh;
-													markh = 1;
-													markv = markv+1;
-												end
-												if(asc >= 8'h61 && asc <= 8'h7a) asc <= upper? asc: asc - 32;
-												else if(asc >= 8'h41 && asc <= 8'h5a) asc <= upper? asc: asc + 32;
-												
-										end
-									end
+									
 								  end
-						  dep2: begin 
-									if(nowd == 8'hf0) begin con <= outdep2; upper = ~upper; end
-									else begin pred = nowd;asc = mem[pred]; 
-											asch <= markh; ascv <= markv;	
-												markh = markh+1;
-												if(markh == 70) begin
-													markh = 1;
-													markv = markv+1;
-												end
-												if(asc >= 8'h61 && asc <= 8'h7a) asc <= upper? asc-32: asc;
-												else if(asc >= 8'h41 && asc <= 8'h5a) asc <= upper? asc+32: asc;
-												
-									end
-								  end
-						  outdep2: begin con <= dep1; 
-										if(indi == 1) begin pred = 8'h12; end
-										end
 						  
-						  outdep1: begin con <= dep0; indi = 0;end
+						  outdep1: begin con <= dep0; indi = 0; asc = 0; end
 						  
 						  default: begin con <= dep0; end
 						endcase
